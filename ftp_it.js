c5baad2458847adb68847fa01b83fcbbf66c9bbd
Richard Herbert <richard@infoweb.co.uk>
2022-04-30 09:46:55 +0100

    Added deploy scripts and example .env file

---
+++ ftp_it.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+const ftp = require("basic-ftp")
+
+upload()
+
+async function upload() {
+  const client = new ftp.Client()
+  client.ftp.verbose = true
+  try {
+    await client.access({
+      host: process.env.FTP_HOST_IT,
+      user: process.env.FTP_USER_IT,
+      password: process.env.FTP_PASSWORD,
+      port: process.env.FTP_DEFAULT_PORT,
+      secure: true,
+      secureOptions: {
+        rejectUnauthorized: false
+      }
+    })
+    console.log(await client.list())
+    await client.ensureDir("")
+    await client.clearWorkingDir()
+    await client.uploadFromDir("public")
+  }
+  catch(err) {
+    console.log(err)
+  }
+  client.close()
+}
